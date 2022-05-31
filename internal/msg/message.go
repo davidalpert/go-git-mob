9dd543c13e309b63ff7c704983811f29eb4f0651
David Alpert <david@spinthemoose.com>
2022-05-31 07:36:31 -0500

    refactor: move Author into the cfg package

--- internal/msg/message.go
+++ internal/msg/message.go
@@ -2,17 +2,13 @@ package msg
 
 import (
 	"fmt"
+	"github.com/davidalpert/go-git-mob/internal/cfg"
 	"strings"
 )
 
-type Author struct {
-	Name  string
-	Email string
-}
-
 // FormatCoAuthorList takes a list of authors in the form "name <email>" and
 // formats them as a newline-separated list of Co-authored-by tags
-func FormatCoAuthorList(coAuthorList []Author) string {
+func FormatCoAuthorList(coAuthorList []cfg.Author) string {
 	tags := make([]string, len(coAuthorList))
 	for i, a := range coAuthorList {
 		tags[i] = fmt.Sprintf("Co-authored-by: %s <%s>", a.Name, a.Email)
