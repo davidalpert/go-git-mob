ff462bc417dfd59cbb5693c83eec5b8662748e7a
David Alpert <david@spinthemoose.com>
2022-07-03 16:34:32 -0500

    test: set GIT_CEILING_DIRECTORIES before all features
    this helps to isolate the git commands in the test specs
    from discovering project-level git configuration and provides
    better overall isolation; features which require git config
    now have to set them up as the project-level GIT_DIR is no
    longer discoverable from within the context of a feature
    step

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,9 +1,7 @@
 require_relative 'os_helper'
 
-Before do
-end
-
-After do
+Before('@announce-paths') do
+  aruba.announcer.activate :paths
 end
 
 Before('@announce-git-log') do
@@ -20,4 +18,20 @@ end
 
 Before('@pending') do
   pending
+end
+
+Before do
+  aruba.announcer.announce(:paths, "aruba - root_directory:    #{aruba.root_directory}")
+  aruba.announcer.announce(:paths, "aruba - working_directory: #{File.join(aruba.root_directory, aruba.config.working_directory)}")
+  aruba.announcer.announce(:paths, "aruba - current_directory: #{File.join(aruba.root_directory, aruba.current_directory)}")
+
+  # ensure that git, when run by aruba, does not accidentally discover
+  # any of the parent repo's git config
+  ceiling_dir = File.dirname(expand_path('.'))
+  aruba.announcer.announce(:paths, "git git ceiling set to:    #{ceiling_dir}         (git won't look for config or .git/ in this folder or above)")
+  # aruba.announcer.announce(:paths, "git won't look for config or .git/ folders in this folder or above")
+  set_environment_variable('GIT_CEILING_DIRECTORIES', ceiling_dir)
+end
+
+After do
 end
