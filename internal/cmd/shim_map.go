c302ecbd5e28907a14fc1eea1d4b02517f9e5c24
David Alpert <david@spinthemoose.com>
2022-08-14 16:34:15 -0500

    refactor: extract the map of shims to it's own file
    makes the map easier to discover and maintain

---
+++ internal/cmd/shim_map.go
@@ -0,0 +1,11 @@
+package cmd
+
+// ShimMap stores the mapping of helper scripts to the equivalent git-mob subcommands; this
+// mapping is used to auto-generate shim scripts and enable simpler syntax for calling these subcommands
+// as if they were their own git plugins
+var ShimMap = map[string]string{
+	"git-mob-print":         "git-mob print",
+	"git-mob-version":       "git-mob version",
+	"git-solo":              "git-mob solo",
+	"git-suggest-coauthors": "git-mob coauthors suggest",
+}
